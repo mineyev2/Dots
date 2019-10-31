1e7663ddd31f6a6c3c20976687bb066f2dc09e54
mineyev2 <mineyev2@illinois.edu>
2019-10-31 13:38:40 -0500

    very basic stuff is now complete

---
+++ DotsGame/Assets/MousePointerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointerMovement : MonoBehaviour
+{
+    //public Rigidbody2D rb;
+    Vector2 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;
+    }
+}
---
+++ DotsGame/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 0.5f;
+
+    public Rigidbody2D rd;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rd.MovePosition(rd.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
